c9b8059ffeb1209721669515bb281338f9cec42b
Benoît D'Angelo <dev@dangelob.eu>
2015-03-24 15:33:47 +0100

    add label equations

---
+++ R/lm_eqn.R
@@ -0,0 +1,31 @@
+#' A fonction to extract p-value from a lm object
+#' 
+#' @param m the lm object from which we want the equation an R2
+#' @param type preselection of returned elements (R2, eq, eq+R2, )
+#' 
+#' @export
+# modified from : http://stackoverflow.com/questions/7549694/ggplot2-adding-regression-line-equation-and-r2-on-graph
+lm_eqr2 = function(m){
+  if (class(m) != "lm") stop("Not an object of class 'lm' ")
+  eq <- substitute(y == a + b %.% x*","~~r^2~"="~r2, 
+                   list(a = format(coef(m)[1], digits = 2), 
+                        b = format(coef(m)[2], digits = 2), 
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));                 
+}
+
+lab_r2pval = function(m){
+  if (class(m) != "lm") stop("Not an object of class 'lm' ")
+  eq <- substitute(R^2~"="~r2*","~p~"<"~pval,
+                   list(pval = format(0.001,digits = 3), 
+                        r2 = format(summary(m)$r.squared, digits = 2)))
+  return(as.character(as.expression(eq)));     
+}
+
+lab_eq = function(m){
+  if (class(m) != "lm") stop("Not an object of class 'lm' ")
+  eq <- substitute(y == a + b %.% x, 
+                   list(a = format(coef(m)[1], digits = 2), 
+                        b = format(coef(m)[2], digits = 2)))
+  return(as.character(as.expression(eq)));     
+}
