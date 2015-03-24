be4849171014e5907e822542ea498143ccd30060
Benoît D'Angelo <dev@dangelob.eu>
2015-03-24 13:38:33 +0100

    set up project

---
+++ R/bdphdtoolbox.R
@@ -0,0 +1,11 @@
+#' bdphdtoolbox: A collection of function that I needed during my PhD
+#'
+#' The bdphdtoolbox package provides two categories of important functions:
+#' modelisation, and other
+#' 
+#' @section modelisation functions:
+#' The modelisation functions ...
+#'
+#' @docType package
+#' @name xtrct_pval
+NULL
