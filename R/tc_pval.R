d2da6c0170e6acb96bc6648ab20aaece99a38689
Benoît D'Angelo <dev@dangelob.eu>
2015-03-25 08:35:40 +0100

    add transcode function for p-value

---
+++ R/tc_pval.R
@@ -0,0 +1,13 @@
+#' A fonction to transcode p-value to significant star
+#' 
+#' @param vec the vector to transcode
+#' 
+#' @export
+
+tc_pval <- function(vec){
+  # Passer la colonne pvalue en factor
+  vec <- cut(vec, breaks=(c(min(vec), 0.001, 0.01, 0.05, max(vec))), right=FALSE)
+  # Changer nom factor
+  levels(vec) <- c("***", "**", "*" ,"-")
+  return(vec)
+}
