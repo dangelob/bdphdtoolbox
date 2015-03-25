38554bc2970f28bfb86cf311a4db1ecd713dfc9f
Benoît D'Angelo <dev@dangelob.eu>
2015-03-25 08:42:37 +0100

    dbg: vec max value returning "NA" instead of "-"

--- R/tc_pval.R
+++ R/tc_pval.R
@@ -6,7 +6,7 @@
 
 tc_pval <- function(vec){
   # Passer la colonne pvalue en factor
-  vec <- cut(vec, breaks=(c(min(vec), 0.001, 0.01, 0.05, max(vec))), right=FALSE)
+  vec <- cut(vec, breaks=(c(min(vec), 0.001, 0.01, 0.05, (max(vec)+1))), right=FALSE)
   # Changer nom factor
   levels(vec) <- c("***", "**", "*" ,"-")
   return(vec)
