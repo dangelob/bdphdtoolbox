6237605096719096ec7d5eb93fead0d98fe524f2
Benoît D'Angelo <dev@dangelob.eu>
2015-09-09 07:52:56 +0200

    dbg what if NA in vector

--- R/tc_pval.R
+++ R/tc_pval.R
@@ -6,7 +6,7 @@
 
 tc_pval <- function(vec){
   # Passer la colonne pvalue en factor
-  vec <- cut(vec, breaks=(c(min(vec), 0.001, 0.01, 0.05, (max(vec)+1))), right=FALSE)
+  vec <- cut(vec, breaks=(c(min(vec, na.rm=T), 0.001, 0.01, 0.05, (max(vec, na.rm=T)+1))), right=FALSE)
   # Changer nom factor
   levels(vec) <- c("***", "**", "*" ,"-")
   return(vec)
